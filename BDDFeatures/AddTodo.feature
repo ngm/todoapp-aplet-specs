722597d7841cae34411a3f959e6ca9b6e3be1e9f
Neil Mather <ngmather@gmail.com>
2017-12-10 23:52:35 +0000

    Write scenarios for add todo

--- BDDFeatures/AddTodo.feature
+++ BDDFeatures/AddTodo.feature
@@ -4,3 +4,15 @@ Feature: Add todo to list
 As a user
 In order to keep track of my todos
 I want to be able to view a list of my todos
+
+Scenario: Add one-word todo
+When I create a todo with the label "Cogitate"
+Then the todo "Cogitate" is added to my todo list
+
+Scenario: Add multi-word todo
+When I create a todo with the label "Take over the world"
+Then the todo "Take over the world" is added to my todo list
+
+Scenario: Add empty todo
+When I create a todo with the label ""
+Then I should get an error saying the label is empty
--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -23,4 +23,45 @@ class AcceptanceTester extends \Codeception\Actor
    /**
     * Define custom actions here
     */
+
+    /**
+     * @When I create a todo with the label :label
+     */
+    public function iCreateATodoWithTheLabel($label)
+    {
+        $this->amOnPage('/add.php?label=' . $label);
+    }
+
+    /**
+     * @Then the todo :label is added to my todo list
+     */
+    public function theTodoIsAddedToMyTodoList($label)
+    {
+        $this->amOnPage('/index.php');
+        $this->see($label);
+    }
+
+    /**
+     * @Given I have created the following todos:
+     */
+    public function iHaveCreatedTheFollowingTodos()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `I have created the following todos:` is not defined");
+    }
+
+    /**
+     * @When I visit my todo list
+     */
+    public function iVisitMyTodoList()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `I visit my todo list` is not defined");
+    }
+
+    /**
+     * @Then I should see my todos
+     */
+    public function iShouldSeeMyTodos()
+    {
+        throw new \Codeception\Exception\Incomplete("Step `I should see my todos` is not defined");
+    }
 }

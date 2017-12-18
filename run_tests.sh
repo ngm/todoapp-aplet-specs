09cec9479a5fa8f1fd8cae4a80d38313927a029a
Neil Mather <ngmather@gmail.com>
2017-12-18 14:15:18 +0000

    Update feature tree script to include test pass/fail colours

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ export APP_DIR=/home/neil/Code/todoapp-aplet/
 cp eclipse/configs/$PRODUCT.config $APP_DIR/todo.config
 export FEATURES=$(sed 's?\(.*\)?-g \1 ?' eclipse/configs/$PRODUCT.config | tr -d '\r\n')
 export NOTFEATURES=$(comm -13 <(sort eclipse/configs/$PRODUCT.config) <(sort eclipse/configs/optionals.config) | sed 's?\(.*\)?-g Not\1 ?' | tr -d '\r\n')
-php vendor/bin/codecept run acceptance $FEATURES $NOTFEATURES --debug --html
+php vendor/bin/codecept run acceptance $FEATURES $NOTFEATURES --debug --json --html --xml
--- scripts/fm_augment.py
+++ scripts/fm_augment.py
@@ -6,10 +6,12 @@ import argparse
 import json
 import pprint
 import hashlib
+import pprint
 
 arg_parser = argparse.ArgumentParser(description='Draw dot file from FeatureIDE model.xml')
 arg_parser.add_argument('model_xml_file', help="Location of the model XML file")
 arg_parser.add_argument('features_dir', help="Location of the feature files")
+arg_parser.add_argument('test_results_file', help="Location of the test results")
 arg_parser.add_argument('output_dir', help="Location where the output file should be rendered")
 arg_parser.add_argument('--output_filename', default="feature_model", help="Name to use for the output file")
 
@@ -35,35 +37,76 @@ for feature_file in listdir(args.features_dir):
                 tags[tag_name] = []
             tags[tag_name].append("S: " + scenario['name'])
 
-
-def parseFeature(feature, parent, graph):
+def parseFeature(feature, parent, graph, test_results):
     feature_name = feature.get("name")
-    bgcolor = "white"
-    if feature.get("abstract") is not None:
-        bgcolor = "#cccccc"
-    graph.attr('node', fillcolor=bgcolor, style='filled', shape='box')
-    graph.node(feature_name, feature_name)
+    has_failed_test = False
+    fill_color = "white"
+
+    for child in feature.getchildren():
+        has_failed_test = parseFeature(child, feature, graph, test_results)
+
+    # add gherkin nodes
     if feature_name in tags:
         tags_for_feature = tags[feature_name]
-        for piece_name in tags_for_feature:
-            piece_hash = hashlib.sha256(piece_name.encode('utf-8')).hexdigest()
-            graph.node(piece_hash, piece_name)
-            graph.edge(feature_name, piece_hash)
+        with graph.subgraph(name="cluster_" + feature_name) as subgraph:
+            feature_name_hash = hashlib.sha256(feature_name.encode('utf-8')).hexdigest()
+            subgraph_root_name = "cluster_" + feature_name_hash
+            subgraph.node(subgraph_root_name, label="", shape="none", width="0", height="0", style="invis")
+            graph.edge(feature_name, subgraph_root_name)
+            subgraph.attr(rankdir="TB")
+            previous = subgraph_root_name
+            for piece_name in tags_for_feature:
+                piece_hash = hashlib.sha256(piece_name.encode('utf-8')).hexdigest()
+                line_color = "#000000"
+                if piece_name[3:] in test_results:
+                    if test_results[piece_name[3:]] == True:
+                        line_color = "#00cc00"
+                    elif test_results[piece_name[3:]] == False:
+                        line_color = "#ff0000"
+                        has_failed_test = True
+                subgraph.node(piece_hash, piece_name, color=line_color, fillcolor="white", shape="box")
+                subgraph.edge(previous, piece_hash, style="invis", weight="0")
+                previous = piece_hash
+
+    # add fmfeature node
+    fillcolor = "white"
+    line_color = "green"
+    if feature.get("abstract") is not None:
+        fillcolor = "#cccccc"
+    if has_failed_test is True:
+        fillcolor = "#ffeeee"
+        line_color = "red"
+    graph.node(feature_name, feature_name, fillcolor=fillcolor, style='filled', shape='box', color=line_color)
     if parent is not None:
         parent_name = parent.get("name")
         arrowhead = "odot"
         if feature.get("mandatory") is not None:
             arrowhead = "dot"
         graph.edge(parent_name, feature_name, arrowhead=arrowhead)
-    for child in feature.getchildren():
-        parseFeature(child, feature, graph)
 
+    return has_failed_test
+
+# Parse tests results
+test_results_file = args.test_results_file #"../tests/_output/report.xml"
+tree = et.parse(test_results_file)
+root = tree.getroot()
+acceptance_suite = root.find('testsuite')
+
+test_results = {}
+for testcase in acceptance_suite:
+    scenario_name = testcase.get("feature")
+    passed = True
+    if testcase.find("failure") is not None:
+        passed = False
+    test_results[scenario_name] = passed
+
+# Parse feature model
 tree = et.parse(args.model_xml_file)
 root = tree.getroot()
 features = root.find('struct')
 graph = gv.Digraph(format="svg")
 
 for feature in features.getchildren():
-    parseFeature(feature, None, graph)
+    parseFeature(feature, None, graph, test_results)
 
 graph.render(filename=path.join(args.output_dir, args.output_filename))

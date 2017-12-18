26c053a1326979aa16f6edd240f8da46a537d510
Neil Mather <ngmather@gmail.com>
2017-12-18 16:36:11 +0000

    Copy test results to report directory

--- run_tests.sh
+++ run_tests.sh
@@ -5,3 +5,7 @@ cp eclipse/configs/$PRODUCT.config $APP_DIR/todo.config
 export FEATURES=$(sed 's?\(.*\)?-g \1 ?' eclipse/configs/$PRODUCT.config | tr -d '\r\n')
 export NOTFEATURES=$(comm -13 <(sort eclipse/configs/$PRODUCT.config) <(sort eclipse/configs/optionals.config) | sed 's?\(.*\)?-g Not\1 ?' | tr -d '\r\n')
 php vendor/bin/codecept run acceptance $FEATURES $NOTFEATURES --debug --json --html --xml
+# copying report file for product
+cp tests/_output/report.json tests/_output/reports/report$PRODUCT.json
+cp tests/_output/report.xml tests/_output/reports/report$PRODUCT.xml
+cp tests/_output/report.html tests/_output/reports/report$PRODUCT.html
